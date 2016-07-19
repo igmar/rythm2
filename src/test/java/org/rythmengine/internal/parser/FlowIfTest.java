ba8b17d56c71a533e877bdc7a0dbbfb58742818f
Igmar Palsenberg <igmar.palsenberg@boostermedia.com>
2016-07-19 16:17:59 +0200

    Add tests for @if

---
+++ src/test/java/org/rythmengine/internal/parser/FlowIfTest.java
@@ -0,0 +1,34 @@
+package org.rythmengine.internal.parser;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.rythmengine.TestBase;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by igmar on 18/07/16.
+ */
+public class FlowIfTest extends TestBase {
+    @Test
+    public void testEmptyBlock() {
+        InputStream is = loadTemplate("flow_if/flow_if1.html");
+        String expected = loadFile("flow_if/flow_if1.html.exp");
+        org.rythmengine.internal.parser.RythmParser parser = createParser(is);
+        ParseTree pt = parser.template();
+        assertEquals(expected, pt.toStringTree(parser));
+    }
+
+    @Test
+    public void testSimpleIf() {
+        InputStream is = loadTemplate("flow_if/flow_if2.html");
+        String expected = loadFile("flow_if/flow_if2.html.exp");
+        org.rythmengine.internal.parser.RythmParser parser = createParser(is);
+        ParseTree pt = parser.template();
+        System.out.println(String.format("XX '%s'", pt.toStringTree(parser)));
+        assertEquals(expected, pt.toStringTree(parser));
+    }
+}
