97b807614424cbd311290b9ba2c72550eb852dd6
Igmar Palsenberg <igmar.palsenberg@coolgames.com>
2017-02-17 08:41:06 +0100

    Add empty testcases

--- src/test/java/org/rythmengine/internal/generator/GeneratorJava7Test.java
+++ src/test/java/org/rythmengine/internal/generator/GeneratorJava7Test.java
@@ -17,7 +17,6 @@ public class GeneratorJava7Test extends TestBase {
         RythmEngine engine = new RythmEngine(config);
 
         InputStream is = loadTemplate("generator/generator1.html");
-        System.out.println("Loaded ");
         String expected = loadFileRaw("generator/generator1.java.generated");
         CompiledTemplate result = engine.compile("generator/generator1.html", is);
         String actual = result.source();
@@ -31,7 +30,6 @@ public class GeneratorJava7Test extends TestBase {
         RythmEngine engine = new RythmEngine(config);
 
         InputStream is = loadTemplate("generator/generator2.html");
-        System.out.println("Loaded ");
         String expected = loadFileRaw("generator/generator2.java.generated");
         CompiledTemplate result = engine.compile("generator/generator2.html", is);
         String actual = result.source();
@@ -45,7 +43,6 @@ public class GeneratorJava7Test extends TestBase {
         RythmEngine engine = new RythmEngine(config);
 
         InputStream is = loadTemplate("generator/generator3.html");
-        System.out.println("Loaded ");
         String expected = loadFileRaw("generator/generator3.java.generated");
         CompiledTemplate result = engine.compile("generator/generator3.html", is);
         String actual = result.source();
@@ -59,9 +56,21 @@ public class GeneratorJava7Test extends TestBase {
         RythmEngine engine = new RythmEngine(config);
 
         InputStream is = loadTemplate("generator/generator4.html");
-        System.out.println("Loaded ");
         String expected = loadFileRaw("generator/generator4.java.generated");
         CompiledTemplate result = engine.compile("generator/generator4.html", is);
+        String actual = result.source();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generatorTest5() {
+        RythmConfiguration config = createRythmConfiguration();
+        RythmEngine engine = new RythmEngine(config);
+
+        InputStream is = loadTemplate("generator/generator5.html");
+        String expected = loadFileRaw("generator/generator5.java.generated");
+        CompiledTemplate result = engine.compile("generator/generator5.html", is);
         String actual = result.source();
 
         assertEquals(expected, actual);
